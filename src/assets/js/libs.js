0ca3c6caa8b28ab6b8edefff32faa7c220e93a70
MarianVytak <marianvytak@gmail.com>
2020-02-04 11:55:31 +0200

    Start Work

---
+++ src/assets/js/libs.js
@@ -0,0 +1,6 @@
+//= ../../../bower_components/jquery/dist/jquery.js
+//= ../../../bower_components/bootstrap/dist/js/bootstrap.bundle.js
+//= ../../../bower_components/twentytwenty/js/jquery.event.move.js
+//= ../../../bower_components/twentytwenty/js/jquery.twentytwenty.js
+//= ../../../bower_components/slick-carousel/slick/slick.js
+//= ../../../bower_components/fancybox/dist/jquery.fancybox.js
